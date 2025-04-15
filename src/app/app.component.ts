ade2264e8a8edbb94be24b993eb4ef442891fbb1
matthieu <matthieu@wegrow-app.com>
2025-04-15 15:37:10 +0200

    feature: add daisyui with custom theme and initiate a default landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+type Toast = {
+  title?: string,
+  content: string,
+  type: "info" | "error" | "warning"
+}
+
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'pieds-croises-front';
+
+  toasts = signal<Toast[]>([])
+  toast: Toast = {
+    title: "STOP IT",
+    content: "Tu vas le regretter ...!",
+    type: "error"
+
+  }
+
+  addToast(toast: Toast) {
+    this.toasts.update((prev) => {
+      const newToasts = prev.length >= 4 ? prev.slice(1) : prev
+      return [...newToasts, toast]
+    })
+
+  }
+
+  removeAllToasts() {
+    this.toasts.set([])
+  }
 }
