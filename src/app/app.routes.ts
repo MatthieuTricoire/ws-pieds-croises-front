7472a81d9a11143ad9ec53250c88a0de17d9e4a5
matthieu <matthieutricoire@gmail.com>
2025-07-09 15:12:00 +0200

    add full layout / main layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,20 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
-import { TestComponent } from './pages/test/test.component';
 import { authGuard } from './chore/guards/auth.guard';
+import { FullLayoutComponent } from './shared/components/layouts/full-layout/full-layout.component';
+import { MainLayoutComponent } from './shared/components/layouts/main-layout/main-layout.component';
+import { TestComponent } from './pages/test/test.component';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginPageComponent },
-  { path: 'test', component: TestComponent, canActivate: [authGuard] },
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [{ path: 'test', component: TestComponent, canActivate: [authGuard] }],
+  },
+  {
+    path: '',
+    component: FullLayoutComponent,
+    children: [{ path: 'login', component: LoginPageComponent }],
+  },
+  { path: '**', redirectTo: '' },
 ];
