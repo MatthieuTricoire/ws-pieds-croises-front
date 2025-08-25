06c338ebf20574b85ac9c20760da7c7deb9c1240
AGATTEPAILLE <amelie.gattepaille@gmail.com>
2025-08-25 16:25:19 +0200

    subscription cards fetch data with http cookie

--- src/app/chore/services/subscription.service.ts
+++ src/app/chore/services/subscription.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { UserSubscription } from '../../shared/models/subscription';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root',
@@ -10,8 +11,17 @@ export class SubscriptionService {
   private apiUrl = 'http://localhost:8080/user-subscriptions';
 
   #http = inject(HttpClient);
+  #auth = inject(AuthService);
 
   getUserSubscriptions(): Observable<UserSubscription[]> {
-    return this.#http.get<UserSubscription[]>(`${this.apiUrl}/user/1`);
+    const user = this.#auth.userSignal();
+    console.log(user);
+    if (!user) {
+      throw new Error('Aucun utilisateur connecté');
+    }
+
+    return this.#http.get<UserSubscription[]>(`${this.apiUrl}/user/${user.id}`, {
+      withCredentials: true,
+    });
   }
 }
