a09be8697d59efe74f45b9a94dd69d0f1b5c3900
Baptiste1828 <baptiste.delefosse@hotmail.fr>
2025-08-28 11:10:37 +0200

    Change of h3 and p tags by app-typography

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -3,11 +3,12 @@ import { Observable } from 'rxjs';
 import { BoxInfo } from '../../models/boxInfo';
 import { BoxService } from '../../../chore/services/box.service';
 import { AsyncPipe } from '@angular/common';
+import { TypographyComponent } from '../design-system/typography/typography.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, TypographyComponent],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'],
 })
