99616ff8040f5e0ec60b2df3618e9f5aab132606
matthieu <matthieutricoire@gmail.com>
2025-10-08 21:34:51 +0200

    update apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   //TODO: changer l'url de l'api une fois déployée
-  apiUrl: 'https://api.monapp.fr',
+  apiUrl: 'pieds-croises-api-xoopnh-95ff0c-31-97-35-53.traefik.me',
 };
