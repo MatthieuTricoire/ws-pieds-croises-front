0016e193969b7200f3f049b45d397396bb452dde
matthieu <matthieutricoire@gmail.com>
2025-10-08 21:44:01 +0200

    test apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   //TODO: changer l'url de l'api une fois déployée
-  apiUrl: 'pieds-croises-api-xoopnh-95ff0c-31-97-35-53.traefik.me',
+  // apiUrl: 'pieds-croises-api-xoopnh-95ff0c-31-97-35-53.traefik.me',
+  apiUrl: 'test',
 };
