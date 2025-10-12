72131fef3481b85e3996f8c260aaf9fd747b9945
matthieu <matthieutricoire@gmail.com>
2025-10-12 20:12:06 +0200

    add preprod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://pieds-croises-api-xoopnh-95ff0c-31-97-35-53.traefik.me',
-  apiUrl: 'https://url-de-prod',
+  apiUrl: 'https://pieds-croises-api-xoopnh-95ff0c-31-97-35-53.traefik.me',
+  // apiUrl: 'https://url-de-prod',
 };
