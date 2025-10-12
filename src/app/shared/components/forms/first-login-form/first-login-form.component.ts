67011980c2ec6cb55c6582007e7d027e0b1a2180
Agattepaille <amelie.gattepaille@gmail.com>
2025-10-12 15:17:18 +0200

    handle error onSubmit

--- src/app/shared/components/forms/first-login-form/first-login-form.component.ts
+++ src/app/shared/components/forms/first-login-form/first-login-form.component.ts
@@ -47,7 +47,7 @@ export class FirstLoginFormComponent implements OnInit {
           },
           error: (error) => {
             console.error('Error:', error);
-            // Gérez les erreurs ici, par exemple, affichez un message d'erreur à l'utilisateur
+            this.#toastService.show('error', 'Une erreur est survenue.', '');
           },
         });
     }
