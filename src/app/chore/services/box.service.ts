dece608e381ed985e87fd15299e41c70c197f98c
Marie-Sail <marie.saillard.pro@gmail.com>
2025-10-14 15:22:09 +0200

    fix: box service

--- src/app/chore/services/box.service.ts
+++ src/app/chore/services/box.service.ts
@@ -13,16 +13,18 @@ export class BoxService {
   boxSignal = signal<BoxInfo | null>(null);
 
   fetchBoxInfo() {
-    this.#http.get<BoxInfo>(`${this.#apiUrl}/box-info`, { withCredentials: true }).subscribe({
+    this.#http.get<BoxInfo>(`${this.#apiUrl}/box/box-info`, { withCredentials: true }).subscribe({
       next: (data) => this.boxSignal.set(data),
       error: (error) => console.error('Error while loading the box', error),
     });
   }
 
   updateBoxInfo(data: BoxInfo) {
-    this.#http.put<BoxInfo>(`${this.#apiUrl}/box-info`, data, { withCredentials: true }).subscribe({
-      next: (updatedBox) => this.boxSignal.set(updatedBox),
-      error: (error) => console.error('Error while updating the box', error),
-    });
+    this.#http
+      .put<BoxInfo>(`${this.#apiUrl}/box/box-info`, data, { withCredentials: true })
+      .subscribe({
+        next: (updatedBox) => this.boxSignal.set(updatedBox),
+        error: (error) => console.error('Error while updating the box', error),
+      });
   }
 }
