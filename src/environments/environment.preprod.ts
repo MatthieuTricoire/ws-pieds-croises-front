caa2558bae87f430c962f9d579a70eafb0f972c5
Agattepaille <amelie.gattepaille@gmail.com>
2025-10-14 18:29:59 +0200

    preprod and test env conf

--- src/environments/environment.preprod.ts
+++ src/environments/environment.preprod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://pieds-croises-preprod.duckdns.org',
+  apiUrl: 'https://pieds-croises-api-preprod.duckdns.org',
 };
--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://pieds-croises-api-mxw6w5-5de73b-31-97-35-53.traefik.me',
+  apiUrl: 'https://pieds-croises-api-test.duckdns.org',
 };
